cb2371b86c440d5992c729ed43639ec84177f86c
Toby Chow <toby_chow@hotmail.com>
2016-10-22 17:47:25 -0400

    add responsiveness

--- test.js
+++ test.js
@@ -1,12 +1,15 @@
+/* jshint esversion:6 */
+$(document).ready(function() {
+
+
+
+
     $(".right").on('click', function(event) {
 
         function addUnicodeBy1(str) {
             return String.fromCharCode(str.charCodeAt() + 1);
         }
 
-        function minusUnicodeBy1(str) {
-            return String.fromCharCode(str.charCodeAt() - 1);
-        }
 
         var slots = {};
 
@@ -37,18 +40,65 @@
                     .animate({
                         left: difference
                             // only if flex is space evenly distributed
-                    }, function() {
+                    }, function cb() {
                         $(target).remove().appendTo(destination).css({
                             left: '0', // resets positioning after inserting img into new DOM
                             position: 'static' // must be static
                         });
                     });
             }
-
-            console.log(`img:${$(this).attr('class')}, initial slot: ${initialSlot}, dataset: ${this.dataset.img}`);
-
             var target = $(this).attr('class');
             var destination = `.slot${this.dataset.img}`;
             animate('.' + target, destination);
         });
     });
+
+
+    $(".left").on('click', function(event) {
+
+        function minusUnicodeBy1(str) {
+            return String.fromCharCode(str.charCodeAt() - 1);
+        }
+
+        var slots = {};
+
+        (function getSlotCoord() {
+            $(".test-container").find("[class*='slot']").each(function(index, el) {
+                slots[index] = { x: $(this).position().left, y: $(this).position().top };
+            });
+        })();
+
+        $(".test-container").find("img").each(function(index, el) {
+
+            var initialSlot = +this.dataset.img;
+            // loops back last img to the first slot
+            if (initialSlot === 0) {
+                this.dataset.img = 4;
+            } else {
+                this.dataset.img = minusUnicodeBy1(this.dataset.img);
+            }
+
+            function animate(target, destination) {
+                var difference = slots[1].x - slots[0].x; // assuming evenly spaced slots
+                $(target)
+                    .css({
+                        'position': 'absolute',
+                        width: '100px',
+                        height: '100px'
+                    })
+                    .animate({
+                        left: '-'+difference
+                            // only if flex is space evenly distributed
+                    }, function cb() {
+                        $(target).remove().appendTo(destination).css({
+                            left: '0', // resets positioning after inserting img into new DOM
+                            position: 'static' // must be static
+                        });
+                    });
+            }
+            var target = $(this).attr('class');
+            var destination = `.slot${this.dataset.img}`;
+            animate('.' + target, destination);
+        });
+    });
+});
